86604f99bfb791ce6c068e731be935b0828480df
imyhacker <arifarhan1602@gmail.com>
2024-09-08 13:29:29 +0700

    Update VPN

--- resources/views/Dashboard/index.blade.php
+++ resources/views/Dashboard/index.blade.php
@@ -1,58 +1,58 @@
 <x-dcore.head />
-  <div id="app">
+<div id="app">
     <div class="main-wrapper main-wrapper-1">
-      <div class="navbar-bg"></div>
-      <x-dcore.nav />
-      <x-dcore.sidebar />
-      <div class="main-content">
-        <section class="section">
-        {{-- <x-dcore.card /> --}}
-
-        <!-- MAIN OF CENTER CONTENT -->
-          <div class="row">
-            <div class="col-lg-12">
-              <div class="card">
-                
-                <div class="card-body text-center">
-                  
-                  <h3>Selamat Datang Di Aplikasi Management Mikrotik ( AMMIK ) AQT Network V.0.1 !</h3>
-                </div>
-              </div>
-              
-            </div>
-            
-
-
-            <div class="col-lg-12">
-              <div class="card">
-                <div class="card-header">
-                  <h6>Tombol Akses Cepat</h6>
-                </div>
-                <div class="card-body text-center">
-                  <div class="row">
-                    <div class="col-md-4 mt-2">
-                      <a href="{{route('datavpn')}}" class="btn btn-primary btn-block">Data VPN</a>
+        <div class="navbar-bg"></div>
+        <x-dcore.nav />
+        <x-dcore.sidebar />
+        <div class="main-content">
+            <section class="section">
+                <!-- MAIN OF CENTER CONTENT -->
+                <div class="row">
+                    <!-- Welcome Card -->
+                    <div class="col-lg-12">
+                        <div class="card wide-card">
+                            <div class="card-body text-center">
+                                <h3>Selamat Datang Di Aplikasi Management Mikrotik ( AMMIK ) AQT Network V.0.1 !</h3>
+                            </div>
+                        </div>
                     </div>
-                    <div class="col-md-4 mt-2">
-                      <a href="{{route('datamikrotik')}}" class="btn btn-primary btn-block">Data Mikrotik</a>
-                    </div>
-                    <div class="col-md-4 mt-2">
-                      <a href="{{route('dataolt')}}" class="btn btn-primary btn-block">Data OLT</a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              
-            </div>
-            
-          </div>
-        <!-- END OF CENTER CONTENT -->
- 
 
-        </section>
-    
-      </div>
-      <x-dcore.footer />
+                    <!-- Quick Access Buttons Card -->
+                    <div class="col-lg-12">
+                        <div class="card wide-card">
+                            <div class="card-header">
+                                <h6>Tombol Akses Cepat</h6>
+                            </div>
+                            <div class="card-body text-center">
+                                <div class="row">
+                                    <div class="col-md-4 mt-2">
+                                        <a href="{{ route('datavpn') }}" class="btn btn-primary btn-block">Data VPN</a>
+                                    </div>
+                                    <div class="col-md-4 mt-2">
+                                        <a href="{{ route('datamikrotik') }}" class="btn btn-primary btn-block">Data Mikrotik</a>
+                                    </div>
+                                    <div class="col-md-4 mt-2">
+                                        <a href="{{ route('dataolt') }}" class="btn btn-primary btn-block">Data OLT</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- END OF CENTER CONTENT -->
+            </section>
+        </div>
+        <x-dcore.footer />
     </div>
-  </div>
+</div>
 <x-dcore.script />
+
+<style>
+    .wide-card {
+        width: 100%; /* Full width */
+    }
+    
+    .wide-card .card-body {
+        padding: 2rem; /* Adjust padding if needed */
+    }
+</style>
