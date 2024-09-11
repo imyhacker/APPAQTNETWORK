9b265e95b4750b1403839f45ba9c622441e063de
imyhacker <arifarhan1602@gmail.com>
2024-09-11 11:22:54 +0700

    Update Baru

--- resources/views/Dashboard/MIKROTIK/dashboardmikrotik.blade.php
+++ resources/views/Dashboard/MIKROTIK/dashboardmikrotik.blade.php
@@ -8,7 +8,40 @@
       <div class="main-content">
         <section class="section">
         {{-- <x-dcore.card /> --}}
-
+        <div class="row">
+           
+          <div class="col-lg-6 col-md-6 col-sm-12">
+            <div class="card card-statistic-2">
+              <div class="card-icon shadow-primary bg-primary">
+                <i class="fas fa-user"></i>
+              </div>
+              <div class="card-wrap">
+                <div class="card-header">
+                  <h4>Total Pengguna Terdaftar</h4>
+                </div>
+                <div class="card-body">
+                  $187,13
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-12">
+            <div class="card card-statistic-2">
+              <div class="card-icon shadow-primary bg-primary">
+                <i class="fas fa-user"></i>
+              </div>
+              <div class="card-wrap">
+                <div class="card-header">
+                  <h4>Total VPN Terbuat</h4>
+                </div>
+                <div class="card-body">
+                  $187,13
+                </div>
+              </div>
+            </div>
+          </div>
+         
+        </div>
         <!-- MAIN OF CENTER CONTENT -->
           <div class="row">
             <div class="col-lg-12">
