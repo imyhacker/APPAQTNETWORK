b359f55434909bbe9f2159ad6dc33d6cb8fa60aa
imyhacker <arifarhan1602@gmail.com>
2024-10-04 13:14:54 +0700

    Update

--- resources/views/Dashboard/IP/aksesschedule.blade.php
+++ resources/views/Dashboard/IP/aksesschedule.blade.php
@@ -67,7 +67,6 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="scheduleModalLabel">Schedule Details</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <p><strong>Name:</strong> <span id="modalName"></span></p>
